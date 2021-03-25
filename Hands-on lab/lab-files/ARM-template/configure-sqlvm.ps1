d21ad9d12c441d017ac498ec512e687d06b53414
AMIT KUMAR <amit.kumar@spektrasystems.com>
2021-03-25 23:47:37 +0530

    Update configure-sqlvm.ps1

--- Hands-on lab/lab-files/ARM-template/configure-sqlvm.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-sqlvm.ps1
@@ -39,10 +39,6 @@ function Add-SqlFirewallRule {
 
 Add-SqlFirewallRule
 
-# Download and install Data Mirgation Assistant
-(New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi', 'C:\DataMigrationAssistant.msi')
-Start-Process -file 'C:\DataMigrationAssistant.msi' -arg '/qn /l*v C:\dma_install.txt' -passthru | wait-process
-
 # Attach the downloaded backup files to the local SQL Server instance
 function Setup-Sql {
     #Add snap-in
@@ -75,8 +71,13 @@ $env:chocolateyUseWindowsCompression = 'true'
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -Verbose
 choco feature enable -n allowGlobalConfirmation
 choco install dotnetfx -y -force
-choco install sql-server-management-studio
+choco install sql-server-management-studio -y -force
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Microsoft SQL Server Management Studio 18.lnk")
+$Shortcut.TargetPath = "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe"
+$Shortcut.Save()
 
+sleep 5
     
 # Download and install Data Mirgation Assistant
 (New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi', 'C:\DataMigrationAssistant.msi')
