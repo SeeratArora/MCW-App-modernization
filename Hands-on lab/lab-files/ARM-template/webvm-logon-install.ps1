4ac7bb0c78b1217a1f58f99adf1277a77c76e8e4
SeeratArora <50323628+SeeratArora@users.noreply.github.com>
2021-03-31 05:43:09 +0530

    Update webvm-logon-install.ps1

--- Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
+++ Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
@@ -42,10 +42,15 @@ Unregister-ScheduledTask -TaskName "Install Lab Requirements" -Confirm:$false
 $env:chocolateyUseWindowsCompression = 'true'
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -Verbose
 choco feature enable -n allowGlobalConfirmation
-choco install dotnetfx -y -force
+#choco install dotnetfx -y -force
 choco install sql-server-management-studio
 
-    
+#.Net 4.8
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://go.microsoft.com/fwlink/?linkid=2088631","C:\ndp48-web.exe")
+Start-Process -file 'C:\ndp48-web.exe' -arg "/q /norestart" 
+sleep 20
+
 # Download and install Data Mirgation Assistant
 (New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi', 'C:\DataMigrationAssistant.msi')
 Start-Process -file 'C:\DataMigrationAssistant.msi' -arg '/qn /l*v C:\dma_install.txt' -passthru | wait-process
